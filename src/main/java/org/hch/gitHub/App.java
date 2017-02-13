d488bd5f745eb5e6ba48186f7e942e3ead867120
huchanghuan <huchanghuan5845@sina.com>
2017-02-13 16:54:31 +0800

    2017/2/13 16:54

--- src/main/java/org/hch/gitHub/App.java
+++ src/main/java/org/hch/gitHub/App.java
@@ -4,10 +4,13 @@ package org.hch.gitHub;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public void run() {
+    	System.out.println("run ~~~~");
     }
 }
--- src/test/java/org/hch/gitHub/AppTest.java
+++ src/test/java/org/hch/gitHub/AppTest.java
@@ -1,15 +1,9 @@
 package org.hch.gitHub;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest {
     /**
      * Create the test case
      *
@@ -21,19 +15,4 @@ public class AppTest
         System.out.println("hello world!");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
